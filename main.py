e53b4365dcc8c12fe16d437b6c74b2f2d41ae7f3
Bernhard Hayden <hello@nini.su>
2024-08-02 14:54:36 +0200

    Add printer function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from weasyprint import HTML, CSS
+import cups
 
 app = FastAPI()
 app.mount("/web", StaticFiles(directory="web", html=True), name="web")
@@ -14,6 +15,7 @@ class Print(BaseModel):
 def print_label(obj_in: Print):
     html_content = generate_html_content(obj_in.name, obj_in.timestamp)
     generate_pdf(html_content)
+    print_file('print/label.pdf', 'QL600')
     return { 'status': 'success'}
 
 def generate_html_content(name: str, timestamp: str) -> str:
@@ -32,3 +34,16 @@ def generate_html_content(name: str, timestamp: str) -> str:
 
 def generate_pdf(html_content: str):
     HTML(string=html_content).write_pdf("print/label.pdf", stylesheets=["print/print-styles.css"])
+
+def print_file(file_path: str, printer_name: str):
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+
+    if printer_name in printers:
+        try:
+            conn.printFile(printer_name, file_path, "Print Job", {})
+            print("File sent to printer successfully.")
+        except cups.IPPError as e:
+            print(f"Error printing file: {e}")
+    else:
+        print(f"Printer '{printer_name}' not found.")
